5d96ca378d46a0f585611843cdb4cc9cd4cb1e83
yamazakiharuki <yamac@yamac.com>
2013-08-08 22:58:12 +0900

    Initial Commit

---
+++ opencv_face_detect_study01/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  opencv_face_detect_study01
+//
+//  Created by YAMAZAKI, *Haruki on 2013/08/08.
+//  Copyright (c) 2013年 YAMAZAKI, *Haruki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
